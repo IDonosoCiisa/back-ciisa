81a616f4a9168799e5446070e02521a10a2e2212
Ignacio Donoso <ignacio.donoso.s@gmail.com>
2024-05-18 01:22:29 -0400

    FEATURE[TERRASOLDIJ] Parcela endpoints implementation

--- api/v2/data/ParcelaController.php
+++ api/v2/data/ParcelaController.php
@@ -13,7 +13,7 @@ class ControladorParcela
     public function getAllParcelas()
     {
         $con = new Conexion();
-        $sql = "select p.nombre, p.parcela_lote_id, pl.nombre as 'nombre_parcela', p.parcela_tipo_id, pt.nombre as 'tipo_parcela', p.numeracion_interna, p.terreno_ancho, p.terreno_largo, p.terreno_despejado_arboles, p.ubicacion_latitud_gm, p.ubicacion_longitud_gm, p.activo from parcela as p inner join parcela_tipo as pt on p.parcela_tipo_id = pt.id inner join parcela_lote as pl on p.parcela_lote_id = pl.id";
+        $sql = "select p.id, p.nombre, p.parcela_lote_id, pl.nombre as 'nombre_parcela', p.parcela_tipo_id, pt.nombre as 'tipo_parcela', p.numeracion_interna, p.terreno_ancho, p.terreno_largo, p.terreno_despejado_arboles, p.ubicacion_latitud_gm, p.ubicacion_longitud_gm, p.activo from parcela as p inner join parcela_tipo as pt on p.parcela_tipo_id = pt.id inner join parcela_lote as pl on p.parcela_lote_id = pl.id";
         $rs = mysqli_query($con->getConnection(), $sql);
         if ($rs) {
             while ($tupla = mysqli_fetch_assoc($rs)) {
@@ -37,7 +37,7 @@ class ControladorParcela
             }
             mysqli_free_result($rs);
         }
-        $sql = "SELECT ps.nombre FROM parcela p inner join parcela_servicio_parcela psp on p.id = psp.parcela_id inner join parcela_servicio ps on ps.id = psp.id where p.id = $idParcela";
+        $sql = "SELECT ps.nombre FROM parcela_servicio ps inner join parcela_servicio_parcela psp on ps.id = psp.parcela_servicio_id where psp.parcela_id = $idParcela";
         $rs = mysqli_query($con->getConnection(), $sql);
         if ($rs) {
             while ($tupla = mysqli_fetch_assoc($rs)) {
@@ -48,6 +48,78 @@ class ControladorParcela
         $con->closeConnection();
 
         return $this->lista;
+    }
+
+    public function newParcela($body)
+    {
+        $con = new Conexion();
+        $sql = "INSERT INTO parcela (nombre, parcela_lote_id, parcela_tipo_id, numeracion_interna, terreno_ancho, terreno_largo, terreno_despejado_arboles, ubicacion_latitud_gm, ubicacion_longitud_gm, pie, valor, activo) VALUES (?,?,?,?,?,?,?,?,?,?,?, false)";
+        $stmt = mysqli_prepare($con->getConnection(), $sql);
+        $rs = $stmt->execute(array($body->nombre, $body->parcela_lote_id, $body->parcela_tipo_id, $body->numeracion_interna, $body->terreno_ancho, $body->terreno_largo, $body->terreno_despejado_arboles, $body->ubicacion_latitud_gm, $body->ubicacion_longitud_gm, $body->pie, $body->valor));
+        $new_id = $stmt->insert_id;
+        $nrows = $stmt->affected_rows;
+        foreach ($body->servicios_parcela as $servicio) {
+            $sql = "INSERT INTO parcela_servicio_parcela (parcela_id, parcela_servicio_id) VALUES (?,?)";
+            $stmt = mysqli_prepare($con->getConnection(), $sql);
+            $rs1 = $stmt->execute(array($new_id, $servicio));
+        }
+        $nrows1 = $stmt->affected_rows;
+        if (!$nrows && !$nrows1) {
+            return false;
+        }
+        return $rs || $rs1;
+    }
+
+    public function patchParcela($body)
+    {
+        $con = new Conexion();
+        $sql = "UPDATE parcela set activo = ? where id = ?";
+        $stmt = mysqli_prepare($con->getConnection(), $sql);
+        $rs = $stmt->execute([$body->activo, $body->id]);
+        $nrows = $stmt->affected_rows;
+        if ($nrows == 0) {
+            return false;
+        }
+        return $rs;
+    }
+    public function putParcela($body)
+    {
+        $con = new Conexion();
+        $sql = "UPDATE parcela set nombre = ?, parcela_lote_id = ?, parcela_tipo_id = ?, numeracion_interna = ?, terreno_ancho = ?, terreno_largo = ?, terreno_despejado_arboles = ?, ubicacion_latitud_gm = ?, ubicacion_longitud_gm = ?, pie = ?, valor = ?  where id = ?;";
+        $stmt = mysqli_prepare($con->getConnection(), $sql);
+        $rs = $stmt->execute(array($body->nombre, $body->parcela_lote_id, $body->parcela_tipo_id, $body->numeracion_interna, $body->terreno_ancho, $body->terreno_largo, $body->terreno_despejado_arboles, $body->ubicacion_latitud_gm, $body->ubicacion_longitud_gm, $body->pie, $body->valor, $body->id));
+        $nrows = $stmt->affected_rows;
+        $sql = "DELETE FROM parcela_servicio_parcela where parcela_id = ?;";
+        $stmt = mysqli_prepare($con->getConnection(), $sql);
+        $rs1 = $stmt->execute(array($body->id));
+        $nrows1 = $stmt->affected_rows;
+        foreach ($body->servicios_parcela as $servicio) {
+            $sql = "INSERT INTO parcela_servicio_parcela (parcela_id, parcela_servicio_id) VALUES (?,?)";
+            $stmt = mysqli_prepare($con->getConnection(), $sql);
+            $rs1 = $stmt->execute(array($body->id, $servicio));
+        }
+        if (!$nrows && !$nrows1) {
+            return false;
+        }
+        return $rs || $rs1;
+    }
+    public function deleteParcela($idHistoria)
+    {
+        $con = new Conexion();
+        $sql = "DELETE FROM parcela_servicio_parcela where parcela_id = ?;";
+        $stmt = mysqli_prepare($con->getConnection(), $sql);
+        $stmt->bind_param("i", $idHistoria);
+        $rs1 = $stmt->execute();
+        $nrows1 = $stmt->affected_rows;
+        $sql = "DELETE FROM parcela where id = ?;";
+        $stmt = mysqli_prepare($con->getConnection(), $sql);
+        $stmt->bind_param("i", $idHistoria);
+        $rs = $stmt->execute();
+        $nrows = $stmt->affected_rows;
+        if (!$nrows && !$nrows1) {
+            return false;
+        }
+        return $rs1 || $rs;
     }
 
 }
--- api/v2/parcela.php
+++ api/v2/parcela.php
@@ -24,6 +24,78 @@ if ($_version == 'v2' && $_api == 'api' && $_endpoint == 'parcela') {
                 echo json_encode(["Error" => "No tiene autorizacion GET"]);
             }
             break;
+        case "POST":
+            if ($_header == $_token_post) {
+                include_once "controller.php";
+                $control = new ControladorParcela();
+                $body = json_decode(file_get_contents("php://input", true));
+                $rs = $control->newParcela($body);
+                if ($rs) {
+                    http_response_code(201);
+                    echo json_encode(["data" => $rs]);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(["data" => "data no válida"]);
+                }
+            } else {
+                http_response_code(401);
+                echo json_encode(["Error" => "No tiene autorizacion POST"]);
+            }
+            break;
+        case "PATCH":
+            if ($_header == $_token_patch) {
+                include_once "controller.php";
+                $control = new ControladorParcela();
+                $body = json_decode(file_get_contents("php://input", true));
+                $rs = $control->patchParcela($body);
+                if ($rs) {
+                    http_response_code(200);
+                    echo json_encode(["data" => $rs]);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(["data" => "data no válida"]);
+                }
+            } else {
+                http_response_code(401);
+                echo json_encode(["Error" => "No tiene autorizacion POST"]);
+            }
+            break;
+        case "PUT":
+            if ($_header == $_token_put) {
+                include_once "controller.php";
+                $control = new ControladorParcela();
+                $body = json_decode(file_get_contents("php://input", true));
+                $rs = $control->putParcela($body);
+                if ($rs) {
+                    http_response_code(200);
+                    echo json_encode(["data" => $rs]);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(["data" => "data no válida"]);
+                }
+            } else {
+                http_response_code(401);
+                echo json_encode(["Error" => "No tiene autorizacion POST"]);
+            }
+            break;
+        case "DELETE":
+            if ($_header == $_token_delete) {
+                include_once "controller.php";
+                $control = new ControladorParcela();
+                $body = json_decode(file_get_contents("php://input", true));
+                $rs = $control->deleteParcela($body->id);
+                if ($rs) {
+                    http_response_code(200);
+                    echo json_encode(["data" => $rs]);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(["data" => "data no válida"]);
+                }
+            } else {
+                http_response_code(401);
+                echo json_encode(["Error" => "No tiene autorizacion DELETE"]);
+            }
+            break;
         default:
             http_response_code(405);
             echo json_encode(["Error" => "No implementado"]);
